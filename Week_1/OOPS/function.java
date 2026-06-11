29947bc2b18bb70da61ecb673cdecdb8f1dccbd5
bhavesh <bhaveshchaudhery915@gmail.com>
2026-06-11 17:08:44 +0530

    day 10 assignment

---
+++ Week_1/OOPS/function.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class function{
+    public static void printmyname(string name){
+        System.out.println(name);
+        return;
+    }
+    public static void main(string[]args){
+        Scanner sc = new Scanner(system.in);
+        string name = sc.next();
+         
+        printmyname(name);
+    }
+}
